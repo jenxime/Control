0475089e001977b4254393596c56c4980d060f02
jenxime <jenifferximena1996@hotmail.com>
2020-07-24 00:35:44 -0500

    cuarto commit

--- Programa1/app.js
+++ Programa1/app.js
@@ -1,5 +1,7 @@
 
 function Imprimir() {
     var name = document.getElementById('nombre').value;
+    var edad = document.getElementById('edad').value;
     alert('bienvenido ' + name);
+    alert('tu edad es ' + edad);
 }
